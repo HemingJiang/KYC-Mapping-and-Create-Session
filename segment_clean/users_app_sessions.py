ff3f24b2aaa4a577f7582e2b310e23370ee76cf1
Heming Jiang <heming@HemingdeMacBook-Pro.local>
2019-02-17 20:38:23 -0500

    Remove files and created readme file

--- segment_clean/users_app_sessions.py
+++
@@ -1,8 +0,0 @@
-import os
-from tendo import singleton
-
-me = singleton.SingleInstance()
-dir_path = '/PATH'
-filename = 'users_app_sessions.sql'
-os.system('psql -h AWS HOST ADDRESS -p 5439 \
-	-f %(d)s/%(f)s DATABASE USER' % {"d": dir_path, "f": filename})
